9c7d45e1f421565ff097ac05fb6e4db9f15a2947
lunarz1741 <fathninrusyda@gmail.com>
2026-01-04 20:31:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ from user_interface import apply_pink_theme, render_math_keyboard,set_background
 apply_pink_theme()
 set_background("images/background.jpg")
 
+# 🎨 UI STYLE FIX (CSS section)
+st.markdown("""
+<style>
+.stButton > button {
+    color: black !important;
+    font-weight: 700 !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
+
 # ----------------- PAGE CONFIG ----------------- #
 st.set_page_config(page_title="DerivaCheck", layout="wide")
 st.markdown('<div class="title-box">🧮 DerivaCheck – Differentiation Step Checker</div>', unsafe_allow_html=True)
